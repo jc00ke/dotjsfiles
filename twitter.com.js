889fcf7b781ebb5bb61e24c4d05137c5da82fca9
jc00ke <jesse@jc00ke.com>
2011-08-18 17:28:47 -0700

    Add F7 to see my retweeted posts

--- twitter.com.js
+++ twitter.com.js
@@ -2,4 +2,11 @@ $().ready(function() {
   setTimeout(function() {
     $('.promo').closest('.component').remove();
   }, 3000);
+
+  $(document).keyup(function(e) {
+    if (e.which == 118) {
+      window.location.hash = "#!/retweeted_of_mine"
+      return false;
+    }
+  });
 });
