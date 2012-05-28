dbd8796f5a6eddcd5825196704aacdea912e0fff
jc00ke <jesse@jc00ke.com>
2012-05-28 12:47:50 -0700

    Show header image on homepage only
    This actually works out alright, as the nav links don't contain
    index.html so the header image is hidden on subsequent pages.

--- mongoid.org.js
+++ mongoid.org.js
@@ -1,7 +1,11 @@
 $().ready(function() {
-  var header = $('#header');
-  header.height('60px');
-  var section = document.location.pathname.split('/')[2];
-  var h = $('<h1>').text(section).css('line-height', 0).css('text-transform', 'capitalize');
-  $('.container', header).append(h);
+  var path = document.location.pathname;
+  var homepage = '/en/mongoid/index.html';
+  if (path != homepage) {
+    var header = $('#header');
+    header.height('60px');
+    var section = path.split('/')[2];
+    var h = $('<h1>').text(section).css('line-height', 0).css('text-transform', 'capitalize');
+    $('.container', header).append(h);
+  }
 });
